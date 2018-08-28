ae2c771effe26bb940381339b4588d1c1a6f70cb
sasank daggubati <sasankdaggubati@gmail.com>
2018-08-27 21:11:16 -0400

    First commit

---
+++ app/components/Popular.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class Popular extends React.Component{
+    render(){
+        var languages = ["All","JavaScript", "Java", "CSS", "Python"];
+        return(
+            <div>
+                Popular!
+            </div>
+        );
+    }
+}
+
+module.exports = Popular;
